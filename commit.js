056a4e95c5ff4d8cf155e3948f8dfcca0bfbc7d3
Samuele Mosna <sam.mosna@gmail.com>
2022-09-22 14:42:07 +0200

    🚀  feat:  testing husky commit-msg

---
+++ commit.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+const commitEmojiCommit = require('./modules/commit-emoji/commit');
+
+commitEmojiCommit()
---
+++ modules/commit-emoji/commit.js
@@ -0,0 +1,25 @@
+module.exports = () => {
+
+    const fs = require("fs");
+
+    let message = fs.readFileSync(process.argv[2], "utf8");
+
+    console.log("> husky-git-emoji parse")
+
+    message = message.replace("revert:", "⏳  revert: ");
+    message = message.replace("build:", "📦  build: ");
+    message = message.replace("ci:", "🤖  ci: ");
+    message = message.replace("docs:", "📖  docs: ");
+    message = message.replace("feat:", "🚀  feat: ");
+    message = message.replace("fix:", "🚑  fix: ");
+    message = message.replace("perf:", "⚡  perf: ");
+    message = message.replace("refactor:", "🚧  refactor: ");
+    message = message.replace("style:", "💄  style: ");
+    message = message.replace("test:", "✅  test: ");
+    message = message.replace("tada:", "🎉  tada: ");
+    message = message.replace("ver:", "🕒  ver: ");
+    message = message.replace("wip:", "🚧  wip: ");
+
+    fs.writeFileSync(process.argv[2], message)
+
+}
