a0a2e7b07dd5653b1e79cf4b34a055c9bf2d91ee
Sam Mosna <info@sammosna.it>
2022-10-07 14:26:29 +0200

    feat: pm check PNPM_PACKAGE_NAME

--- inc/pm.js
+++ inc/pm.js
@@ -2,7 +2,7 @@
  * get package manager
  */
 const pm = () => {
-    if (process.env.PNPM_HOME) {
+    if (process.env.PNPM_HOME || process.env.PNPM_PACKAGE_NAME) {
         return "pnpm";
     } else {
         return "npm";
