734c40629957f9c5281e2ac52ad30e81d5a071be
Samuele Mosna <samuele@hgblu.com>
2023-11-06 13:49:07 +0100

    fix: rc schema path

--- modules/smghrc-init.js
+++ modules/smghrc-init.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 const { smghrcPath } = require("../constants")
 
 const smgh = {
-    "$schema": "https://raw.githubusercontent.com/sammosna/git-hooks/inc/schemas/rc/v1.json"
+    "$schema": "https://raw.githubusercontent.com/sammosna/git-hooks/main/inc/schemas/rc/v1.json"
 }
 
 
